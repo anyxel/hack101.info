ae78b93e50d7a89bc854fdd10b42a46a95b05ee0
Md Obydullah <hi@obydul.me>
2024-05-02 18:24:10 +0600

    add dnsrecon

--- src/components/common/Seo.jsx
+++ src/components/common/Seo.jsx
@@ -14,7 +14,7 @@ export const Seo = ({ title, description, pathname, children }) => {
     twitterUsername,
   }
 
-  let setTitle = seo.title ? `${seo.title} | ${appName}` : appName
+  const setTitle = seo.title ? `${seo.title} | ${appName}` : appName
 
   return (
     <>
@@ -27,7 +27,8 @@ export const Seo = ({ title, description, pathname, children }) => {
       <meta name="twitter:description" content={seo.description} />
       <meta name="twitter:image" content={seo.image} />
       <meta name="twitter:creator" content={seo.twitterUsername} />
-      <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>👤</text></svg>" />
+      <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>🧰</text></svg>" />
+
       {children}
     </>
   )
